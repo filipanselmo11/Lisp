37f66b2218e2bffb42e52c94f1c1d499def0f6c8
filipanselmo11 <filip_anselmo@yahoo.com.br>
2017-09-12 22:29:53 -0400

    Add files via upload

---
+++ exemplo1.lisp
@@ -0,0 +1,10 @@
+(print(+ 3 4))
+(print(+ 4 5))
+(print(+ 5 5 5 5))
+
+(print(<= 2 3))
+(print(> 3 4))
+(print(and 2 t))
+(print(and 2 nil))
+
+
---
+++ exemplo2.lisp
@@ -0,0 +1,18 @@
+(print'(+ 1 2))
+(print'(a b c d e f))
+
+(print(+ pi 0.004))
+(print(+ pi 4.0s-3))
+(print(+ pi 4.0f-3))
+
+(print(length "Oi eu sou o Goku"))
+(print(length "Meu nome é Fílip Anselmo"))
+
+(print(char "Oi eu sou o Goku" 2))
+(print(char "Meu nome é Fílip Anselmo" 5))
+
+(print(char=(char "Uma string" 2)(char "Outra" 4)))
+
+
+
+
