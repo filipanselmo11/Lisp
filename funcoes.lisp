eddf2b580577ed4e0672e82a26244beff8f72c95
filipanselmo11 <filip_anselmo@yahoo.com.br>
2017-09-12 23:10:07 -0400

    Add files via upload

---
+++ funcoes.lisp
@@ -0,0 +1,19 @@
+(defun teste (a b)
+	(* 2(+ a b))
+)
+
+(print(teste 2 3))
+(print(teste 3 4))
+(print(teste 5 5))
+
+
+(defun soma(a b c)
+	(+ a b c)
+)
+
+(print(soma 2 3 4))
+(print(soma 21 32 43))
+(print(soma 23 322 43))
+(print(soma 233 322 1114))
+
+
